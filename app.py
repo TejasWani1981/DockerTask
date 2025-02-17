2463ff2fb897a35404b336b63d7ca13270db0ed3
Tejas.Wani <Tejas.Wani@igtsolutions.com>
2025-02-17 10:33:06 +0530

    Generating pull request after changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def home():
-   return "Hello, welcome to Flask Server by TejasWani for docker task!"
+   return "Hello, welcome to Flask Server by TejasWani for docker task!Changes done for pull request"
  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
